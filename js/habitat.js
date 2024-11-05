3d7063e4a491621d4e3042277c34b608e30ec5f2
julien-35 <julien45.dubois@gmail.com>
2024-11-05 13:31:42 +0100

    t

--- js/habitat.js
+++ js/habitat.js
@@ -39,7 +39,7 @@ async function voirAnimal(habitatId, animalContainer) {
             
             // Style pour l'image
             imageElement.setAttribute('style', `
-                width: 50%;
+                width: 100%;
                 height: 200px;
                 object-fit: cover;
             `);
